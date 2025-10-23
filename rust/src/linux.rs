637f3f1e4c8dbfda028c121663a070bd1433361f
Tran Van Minh TRi <tvmthtr@gmail.com>
2025-10-23 21:17:09 +0700

    udpate

--- rust/src/linux.rs
+++ rust/src/linux.rs
@@ -3,18 +3,18 @@ pub fn press(key: &str) {
     match key {
         "1" => run_command("xdg-open 'https://chatgpt.com/?temporary-chat=true'"),
         "2" => run_command("xdg-open \"https://www.google.com/search?q=$(wl-paste)\""),
-        "3" => run_command("qdbus org.kde.KWin /KWin org.kde.KWin.nextDesktop"),
+        "3" => run_command("qdbus6 org.kde.KWin /KWin org.kde.KWin.nextDesktop"),
         "4" => run_app("kitty"),
         "5" => run_app(
             "sh -c 'tmp=$(mktemp /tmp/clipXXXX.md); wl-paste > $tmp; kitty -e nvim -c \"autocmd VimLeave * call delete(\\\"$tmp\\\")\" $tmp'",
         ),
-        "6" => run_command("qdbus org.kde.KWin /KWin org.kde.KWin.previousDesktop"),
+        "6" => run_command("qdbus6 org.kde.KWin /KWin org.kde.KWin.previousDesktop"),
         "7" => run_command("xdg-open \"https://www.youtube.com/\""),
         "8" => run_command("xdg-open \"https://onedrive.live.com/?view=0\""),
         "9" => run_app("mpv \"$(wl-paste)\""),
         "*" => run_app("kitty -e nvim ~/Documents/Obsidian/"),
         "0" => run_app("/home/mintori/.config/rofi/launchers/rofi-power-menu.sh"),
-        "#" => run_command("qdbus org.freedesktop.ScreenSaver /ScreenSaver Lock"),
+        "#" => run_command("qdbus6 org.freedesktop.ScreenSaver /ScreenSaver Lock"),
         _ => println!("[WARN] Unmapped key: {}", key),
     }
 }
